ca3126e620456c3e95302c1080ba9bdccbb8f65e
Jacek Grochowina <jacek99@vp.pl>
2020-02-01 17:38:20 +0100

    create basic structure, import components

---
+++ src/layouts/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+/* Fonts Awesome */
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import {
+  faChevronLeft,
+  faMobileAlt,
+  faEnvelope,
+  faMapMarkerAlt,
+} from '@fortawesome/free-solid-svg-icons';
+
+/* Styles */
+import '../styles/main.css';
+
+/* Layouts */
+import Header from './Header';
+import Navigation from './Navigation';
+import Page from './Page';
+import Footer from './Footer';
+
+class App extends Component {
+
+  render() {
+
+    library.add(
+      fab,
+      faChevronLeft,
+      faMobileAlt,
+      faEnvelope,
+      faMapMarkerAlt,
+    );
+
+    return (
+      <Router>
+        <div className="app">
+
+          {<Header />}
+          {/* {<Navigation />} */}
+
+          <main>
+            {/* {<Page />} */}
+          </main>
+
+          <footer>
+            {/* {<Footer />} */}
+          </footer>
+
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
