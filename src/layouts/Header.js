1549a2c434d0a454fbf9e6fbe58f10a7d0928fe4
Jacek Grochowina <jacek99@vp.pl>
2020-02-03 10:59:46 +0100

    fix href

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
                         <FontAwesomeIcon icon={['fas', 'envelope']} />
                         <span>apepwape@gmail.com</span>
                     </a>
-                    <a className="header__link" href="https://www.google.com/maps/dir//Sk%C5%82adowa+3,+87-810+W%C5%82oc%C5%82awek/@52.65502,19.0838714,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x471c9bd7863642c7:0x7c8373b38a0ac564!2m2!1d19.0860601!2d52.65502" target="_blank">
+                    <a className="header__link" href="https://www.google.com/maps/dir//Sk%C5%82adowa+3,+87-810+W%C5%82oc%C5%82awek/@52.65502,19.0838714,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x471c9bd7863642c7:0x7c8373b38a0ac564!2m2!1d19.0860601!2d52.65502" target="_blank" rel="noopener noreferrer">
                         <FontAwesomeIcon icon={['fas', 'map-marker-alt']} />
                         <span>Włocławek</span>
                     </a>
