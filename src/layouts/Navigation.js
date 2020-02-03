04981bddf3798513960c1b37699b5718a4637823
Jacek Grochowina <jacek99@vp.pl>
2020-02-03 11:00:38 +0100

    create navigation structure

---
+++ src/layouts/Navigation.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import 'hamburgers/_sass/hamburgers/hamburgers.scss';
+
+import Logo from '../images/logo.svg';
+
+const navList = [
+    { name: "domowa", path: "/", exact: true },
+    { name: "oferta", path: "/oferta" },
+    { name: "realizacje", path: "/realizacje" },
+    { name: "kontakt", path: "/#contact" },
+]
+
+class Navigation extends Component {
+
+    state = {
+        active: false,
+    }
+
+    handleClick = () => {
+        this.setState(prevState => ({
+            active: !prevState.active
+        }));
+    }
+
+    render() {
+        /* Generate list of links */
+        const menu = navList.map(item => (
+            <li key={item.name} className="nav__item">
+                <NavLink to={item.path} exact={item.exact ? item.exact : false} className="nav__link">{item.name}</NavLink>
+            </li>
+        ))
+        return (
+            <nav className="nav" >
+                <div className={this.state.active ? "container nav__container row is-active" : "container nav__container row"} >
+                    <div className="wraper row">
+                        <Link to="/" className="nav__logo">
+                            <img src={Logo} alt="" className="nav__image" />
+                        </Link>
+                        <button className={this.state.active ? "hamburger hamburger--collapse is-active" : "hamburger hamburger--collapse"} type="button" onClick={this.handleClick}>
+                            <span className="hamburger-box">
+                                <span className="hamburger-inner"></span>
+                            </span>
+                        </button>
+                    </div>
+                    <ul className={this.state.active ? "nav__list row is-active" : "nav__list row"}>
+                        {menu}
+                    </ul>
+                </div>
+            </nav >
+        );
+    }
+}
+
+export default Navigation;
