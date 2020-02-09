a87677edea8154f733ec160b461d0c9d9f41fc4f
Jacek Grochowina <jacek99@vp.pl>
2020-02-09 23:25:21 +0100

    create component

---
+++ src/layouts/Footer.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import GoogleMap from '../components/GoogleMap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Footer = () => {
+    return (
+        <div className="footer" id="contact">
+            <div className="container footer__container">
+                <div className="footer__wraper">
+                    <h1 className="header footer__header">Bądźmy w kontakcie</h1>
+                    <div className="row">
+                        <div className="col-6 col-12-sm">
+                            <h2 className="footer__h2">APE Piotr Wysocki</h2>
+                            <ul className="footer__list">
+                                <li className="footer__item"><div className="footer__icon"><FontAwesomeIcon icon={['fas', 'map-marker-alt']} /></div>ul. Wyszyńskiego 19 / 3</li>
+                                <li className="footer__item"><div className="footer__icon"><FontAwesomeIcon icon={['fas', 'map-marked-alt']} /></div> 87-800 Włocławek</li>
+                                <li className="footer__item"><div className="footer__icon"><FontAwesomeIcon icon={['fas', 'envelope']} /></div>apepwape@gmail.com</li>
+                                <li className="footer__item"><div className="footer__icon"><FontAwesomeIcon icon={['fas', 'clock']} /></div>Po godzinach...</li>
+                            </ul>
+                        </div>
+                        <div className="col-6 col-12-sm">
+                            <h2 className="footer__h2">Warsztat - obsługa klienta</h2>
+                            <ul className="footer__list">
+                                <li className="footer__item"><div className="footer__icon"><FontAwesomeIcon icon={['fas', 'map-marker-alt']} /></div>ul. Składowa 3</li>
+                                <li className="footer__item"><div className="footer__icon"><FontAwesomeIcon icon={['fas', 'map-marked-alt']} /></div>87-800 Włocławek</li>
+                                <li className="footer__item"><div className="footer__icon"><FontAwesomeIcon icon={['fas', 'mobile-alt']} /></div>+48 603 915 306</li>
+                                <li className="footer__item"><div className="footer__icon"><FontAwesomeIcon icon={['fas', 'dollar-sign']} /></div>mBank 52114020040000310250449110</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="map footer__map">
+                <GoogleMap />
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
