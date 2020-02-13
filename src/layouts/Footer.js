eaa489b7aa67f7bfaeda123f2998c1ce5785305c
Jacek Grochowina <jacek99@vp.pl>
2020-02-13 18:31:36 +0100

    add footer and link to rodo page

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import GoogleMap from '../components/GoogleMap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -15,7 +16,7 @@ const Footer = () => {
                                 <li className="footer__item"><div className="footer__icon"><FontAwesomeIcon icon={['fas', 'map-marker-alt']} /></div>ul. Wyszyńskiego 19 / 3</li>
                                 <li className="footer__item"><div className="footer__icon"><FontAwesomeIcon icon={['fas', 'map-marked-alt']} /></div> 87-800 Włocławek</li>
                                 <li className="footer__item"><div className="footer__icon"><FontAwesomeIcon icon={['fas', 'envelope']} /></div>apepwape@gmail.com</li>
-                                <li className="footer__item"><div className="footer__icon"><FontAwesomeIcon icon={['fas', 'clock']} /></div>Po godzinach...</li>
+                                <li className="footer__item"><Link to="/"><div className="footer__icon"><FontAwesomeIcon icon={['fas', 'clock']} /></div>Po godzinach...</Link></li>
                             </ul>
                         </div>
                         <div className="col-6 col-12-sm">
@@ -33,6 +34,12 @@ const Footer = () => {
             <div className="map footer__map">
                 <GoogleMap />
             </div>
+            <footer>
+                <div className="container">
+                    <p className="footer__p">APE Piotr Wysocki &copy; 2020 All rights reserved</p>
+                    <Link to="/rodo" className="footer__p footer__p--link">Polityka RODO</Link>
+                </div>
+            </footer>
         </div>
     )
 }
