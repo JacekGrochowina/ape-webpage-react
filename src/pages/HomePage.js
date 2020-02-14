0ef14454303c1f3191fb6cb7876c58a3c9536a11
Jacek Grochowina <jacek99@vp.pl>
2020-02-14 12:57:56 +0100

    add props

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,17 +4,6 @@ import SwiperMain from '../components/SwiperMain';
 import Button from '../components/Button';
 import Parallax from '../components/Paralax';
 
-/* Images to Swiper */
-import Bg1 from '../images/slider/1.jpg';
-import Bg2 from '../images/slider/2.jpg';
-import Bg3 from '../images/slider/3.jpg';
-import Bg4 from '../images/slider/4.jpg';
-import Bg5 from '../images/slider/5.jpg';
-import Bg6 from '../images/slider/6.jpg';
-import Bg7 from '../images/slider/7.jpg';
-import Bg8 from '../images/slider/8.jpg';
-import Bg9 from '../images/slider/9.jpg';
-
 /* Image to parallax */
 import Image from '../images/parallax/fotowoltaika.jpg';
 
@@ -27,68 +16,10 @@ import Icon5 from '../images/services/icon5.png';
 import Icon6 from '../images/services/icon6.png';
 import Icon7 from '../images/services/icon7.png';
 
-const HomePage = () => {
-
-    /* Data of Swiper */
-    const swiperData = [
-        {
-            id: 1,
-            title: "Automatyka przemysłowa",
-            bg: Bg1,
-            link: "/",
-        },
-        {
-            id: 2,
-            title: "Instalacje fotowoltaiczne",
-            bg: Bg2,
-            link: "/",
-        },
-        {
-            id: 3,
-            title: "Elektryczne systemy grzewcze",
-            bg: Bg3,
-            link: "/",
-        },
-        {
-            id: 4,
-            title: "Termowizja",
-            bg: Bg4,
-            link: "/",
-        },
-        {
-            id: 5,
-            title: "Detekcja gazu",
-            bg: Bg5,
-            link: "/",
-        },
-        {
-            id: 6,
-            title: "Drzwi automatyczne",
-            bg: Bg6,
-            link: "/",
-        },
-        {
-            id: 7,
-            title: "Instalacje alarmowe - monitoring",
-            bg: Bg7,
-            link: "/",
-        },
-        {
-            id: 8,
-            title: "Ocena maszyn - wymagania minimalne BHP",
-            bg: Bg8,
-            link: "/",
-        },
-        {
-            id: 9,
-            title: "Strony internetowe",
-            bg: Bg9,
-            link: "/",
-        },
-    ]
+const HomePage = (props) => {
 
     /* Data of icons */
-    const servicesData = [
+    const servicesIconsData = [
         {
             id: 1,
             title: "drzwi automatyczne",
@@ -133,8 +64,8 @@ const HomePage = () => {
         },
     ]
 
-    const servicesIcons = servicesData.map(item => (
-        <Link to="/" className="home__service" key={item.id}>
+    const servicesIcons = servicesIconsData.map(item => (
+        <Link to={servicesIconsData.link} className="home__service" key={item.id}>
             <img className="home__service-icon" src={item.icon} alt={item.title} />
             <h3 className="home__service-icon-title">{item.title}</h3>
         </Link>
@@ -142,7 +73,8 @@ const HomePage = () => {
 
     return (
         <div className="home">
-            <SwiperMain data={swiperData} />
+            {/* props from Page.js > SERVICES_DATA array */}
+            <SwiperMain data={props.data} />
 
             <section>
                 <div className="container home__container home__container--about">
