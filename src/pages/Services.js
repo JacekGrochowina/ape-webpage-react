444a755b63948504ce95425050cdbf93646795a9
Jacek Grochowina <jacek99@vp.pl>
2020-02-14 12:58:31 +0100

    create structure

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Services = () => {
+const Services = (props) => {
+
+    const services = props.data.map(service => (
+        <Link to={service.link}
+            className="wraper services__wraper col-4 col-6-lg col-12-sm"
+            style={{ backgroundImage: `url(${service.bg})` }}>
+            <div>
+                <h1 className="title services__title">{service.title}</h1>
+            </div>
+        </Link>
+    ))
+
     return (
-        <div className="services">
-            <h1>Services</h1>
+        <div className="services row">
+            {services}
         </div>
     )
 }
