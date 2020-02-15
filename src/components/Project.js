c70d7ef249f6ff80014cb281e43da3954082521c
Jacek Grochowina <jacek99@vp.pl>
2020-02-15 18:50:21 +0100

    create structure

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Project = (props) => {
     return (
         <>
-            <h1 className="title">{props.data[props.id - 1].title}</h1>
+            <h1 className="title projects__title--subpage-big">{props.data[props.id - 1].title}</h1>
+
+            <div className="wraper wraper--info">
+                <h4 className="projects__h4">
+                    <FontAwesomeIcon icon={['fas', 'map-marker-alt']} />
+                    {props.data[props.id - 1].place}
+                </h4>
+
+                <h4 className="projects__h4">
+                    <FontAwesomeIcon icon={['fas', 'calendar-alt']} />
+                    {props.data[props.id - 1].data}
+                </h4>
+            </div>
+
+            <p className="project__p">{props.data[props.id - 1].desc}</p>
         </>
     )
 }
