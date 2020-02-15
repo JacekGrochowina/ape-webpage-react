a0146bdfe72f267cb1e609aec9d8365a41ae07ed
Jacek Grochowina <jacek99@vp.pl>
2020-02-15 19:09:57 +0100

    add gallery

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Project = (props) => {
+
+    const gallery = props.data[props.id - 1].images.map(e => (
+        <div className="project__wraper-img col-4 col-6-md col-12-sm">
+            <img className="project__img" src={e} alt={props.data[props.id - 1].title} />
+        </div>
+    ))
+
     return (
         <>
             <h1 className="title projects__title--subpage-big">{props.data[props.id - 1].title}</h1>
@@ -19,6 +26,10 @@ const Project = (props) => {
             </div>
 
             <p className="project__p">{props.data[props.id - 1].desc}</p>
+
+            <div className="project__gallery row">
+                {gallery}
+            </div>
         </>
     )
 }
