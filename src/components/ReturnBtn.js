2aa91ff0114e2a1a8ad875e7017b34d0437b06f2
Jacek Grochowina <jacek99@vp.pl>
2020-02-18 19:50:24 +0100

    add props

--- src/components/ReturnBtn.js
+++ src/components/ReturnBtn.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const ReturnBtn = () => {
+const ReturnBtn = (props) => {
     return (
-        <Link to="/realizacje" className="return-btn">
+        <Link to={props.backTo} className="return-btn">
             <FontAwesomeIcon icon={['fas', 'chevron-left']} />
             Wstecz
         </Link>
