608a96c23f5879e0d6a3398b0e1fff1e1d69d095
Jacek Grochowina <jacek99@vp.pl>
2020-02-27 12:22:41 +0100

    remove data object

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,64 +7,9 @@ import Parallax from '../components/Paralax';
 /* Image to parallax */
 import Image from '../images/parallax/fotowoltaika.jpg';
 
-/* Icons of services */
-import Icon1 from '../images/services/icon1.png';
-import Icon2 from '../images/services/icon2.png';
-import Icon3 from '../images/services/icon3.png';
-import Icon4 from '../images/services/icon4.png';
-import Icon5 from '../images/services/icon5.png';
-import Icon6 from '../images/services/icon6.png';
-import Icon7 from '../images/services/icon7.png';
-
 const HomePage = (props) => {
 
-    /* Data of icons */
-    const servicesIconsData = [
-        {
-            id: 1,
-            title: "drzwi automatyczne",
-            icon: Icon1,
-            link: "/drzwi-automatyczne"
-        },
-        {
-            id: 2,
-            title: "termowizja instalacji",
-            icon: Icon2,
-            link: "/termowizja"
-        },
-        {
-            id: 3,
-            title: "instalacje alarmowe",
-            icon: Icon3,
-            link: "/instalacje-alarmowe"
-        },
-        {
-            id: 4,
-            title: "monitoring obiektów",
-            icon: Icon4,
-            link: "/instalacje-alarmowe"
-        },
-        {
-            id: 5,
-            title: "dyrektywy WE",
-            icon: Icon5,
-            link: "/ocena-maszyn"
-        },
-        {
-            id: 6,
-            title: "ocena ryzyka",
-            icon: Icon6,
-            link: "/ocena-maszyn"
-        },
-        {
-            id: 7,
-            title: "tworzenie stron",
-            icon: Icon7,
-            link: "/tworzenie-stron"
-        },
-    ]
-
-    const servicesIcons = servicesIconsData.map(item => (
+    const servicesIcons = props.data.map(item => (
         <Link to={item.link} className="home__service" key={item.id}>
             <img className="home__service-icon" src={item.icon} alt={item.title} />
             <h3 className="home__service-icon-title">{item.title}</h3>
