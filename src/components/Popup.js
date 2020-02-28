ab85ee619bebddaab3588f8a191da07a72cd7ece
Jacek Grochowina <jacek99@vp.pl>
2020-02-28 16:37:22 +0100

    remove link

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import LogoSelfa from '../images/logo-selfa.svg';
-// import { Link } from 'react-router-dom';
 
 const Popup = () => {
     return (
         <div className="popup">
             <div className="container popup__container row">
-                <a href="http://www.selfa-pv.com/moduly-fotowoltaiczne" target="_blank" rel="noopener noreferrer" className="popup__logo">
+                <div className="popup__logo">
                     <img className="popup__image" src={LogoSelfa} alt="" />
-                </a>
+                </div>
                 <div className="popup__text row">
                     <h3 className="popup__h3">Jesteśmy partnerem handlowym firmy Selfa, polskiego producenta paneli fotowoltaicznych.</h3>
                 </div>
